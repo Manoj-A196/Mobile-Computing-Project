626e7cad23dc4624c844d813320e18b2307a0a21
Manoj-A196 <manojashok0619@gmail.com>
2025-12-27 11:59:48 +0530

    Update devices.py

--- devices.py
+++ devices.py
@@ -1,1 +1,16 @@
+import streamlit as st
 
+st.title("Device Control")
+
+device = st.selectbox(
+    "Select Device",
+    ["Air Conditioner", "Fan", "Light", "Washing Machine"]
+)
+
+power = st.toggle("Power ON / OFF")
+
+mode = st.selectbox("Mode", ["Normal", "Eco", "Turbo"])
+
+temp = st.slider("Temperature", 16, 30)
+
+st.success(f"{device} updated successfully (Demo)")
