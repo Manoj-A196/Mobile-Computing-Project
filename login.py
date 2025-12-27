cfb88bbac604915a6556be9723c98d08f4548299
Manoj-A196 <manojashok0619@gmail.com>
2025-12-27 11:59:05 +0530

    Implement basic login functionality with Streamlit

--- login.py
+++ login.py
@@ -1,1 +1,12 @@
+import streamlit as st
 
+st.title("Login")
+
+username = st.text_input("Username")
+password = st.text_input("Password", type="password")
+
+if st.button("Login"):
+    if username and password:
+        st.success("Login Successful (Demo)")
+    else:
+        st.error("Please enter all details")
