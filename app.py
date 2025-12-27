5b6a4bb08dd5096afea44d0242d564ebc2080ad5
Manoj-A196 <manojashok0619@gmail.com>
2025-12-27 11:58:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
+import streamlit as st
 
+st.set_page_config(page_title="Smart Energy Scheduler", layout="wide")
+
+st.sidebar.title("Navigation")
+
+page = st.sidebar.radio(
+    "Go to",
+    ["Login", "Home", "Devices", "Analytics"]
+)
+
+if page == "Login":
+    import login
+elif page == "Home":
+    import home
+elif page == "Devices":
+    import devices
+elif page == "Analytics":
+    import analytics
