026006e00a81d94604eb38e920613f6a083c5928
Manoj-A196 <manojashok0619@gmail.com>
2025-12-27 11:59:28 +0530

    Update home.py

--- home.py
+++ home.py
@@ -1,1 +1,9 @@
+import streamlit as st
 
+st.title("Home Dashboard")
+
+st.metric("Connected Devices", "4")
+st.metric("Current Power Usage", "1402 W")
+st.metric("Energy Saved Today", "25%")
+
+st.info("Suggestion: Enable Eco Mode to save more energy")
